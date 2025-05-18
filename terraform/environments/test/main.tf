33bcec33f008125583789e3a6591becf7edc118d
heckofagator <aaronheck@gmail.com>
2025-05-18 15:54:10 -0400

    editing virtual machine config

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -10,7 +10,7 @@ terraform {
     storage_account_name = "tfstate7342"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    access_key           = "change_me"
+    access_key           = "HQELeKS13uDIMyetSUvjqjsZLoQRsQ3hBOsqKZ79ZYuH+D7/I9wJAjb680UD9HZyXQ7VXg2K7gPH+AStZUFwTA=="
   }
 }
 module "resource_group" {
--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_interface" "" {
-  name                = ""
+  name                = "udacity-final-nic"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
 
@@ -12,18 +12,18 @@ resource "azurerm_network_interface" "" {
 }
 
 resource "azurerm_linux_virtual_machine" "" {
-  name                = "${var.resource_group_name}-vm"
+  name                = "udacity-final-vm"
   location            = "${var.location}"
-  resource_group_name = "${var.resource_group_name}"
+  resource_group_name = "${var.resource_group}"
   size                = "Standard_DS2_v2"
   admin_username      = ""
-  network_interface_ids = []
+  network_interface_ids = [azurerm_network_interface.udacity-final-nic]
   admin_ssh_key {
     username   = "adminuser"
     public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC2Pn+dZER8DR4U7HZB6WLHlixEPuRiBeCNbv0T23s2Jmur4NKJt/E27hio642TCJNNIXcI7RoXo+CcCJbI8X+O4s4picX2mvdxT7DBAJh3qF01cQ4RnVK/DEqi85E+4y+1FH73wG10+WdaRrJZcWD4NC8tx3XuUVQ3eS7FZjXWW4zKXkXN9auzJagpl+rQWzp+Hq0P3ENVSww6az1YzNYfqSs/n4v/ZAzXrws8lShI5uP6HGkw29Pr+tGYZN6hDRJiSr/grlk9p7N2eCE+EFDL6VOXW1C+0xerFTX+2yq3wVtBjAtNb5IjXwtBB+rZOkx7cnXM1Kmwsa+jFI2+moGhAbrxRBBbHUegGbSJBJzNq58naMD1aMLyweZpmB++Kq5xZVGBAdjqCxnnyqdoChs11meW3VImTDkYziWF9+Lx37WSFT63rOXrOC2S/S7cH2ApQKnD3gSMupa4ick7EWqghxMNWQ0oYdet9l7gnPvme5rdmVqJyWmJCHd00aFqvJM= aaron@Aaron-Home-PC"
   }
   os_disk {
-    caching           = "ReadWrite"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
   source_image_reference {
